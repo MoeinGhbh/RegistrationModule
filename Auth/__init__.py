3613052081947ddb10fb2ed4d8c20f4eb1b893c8
= <=>
2020-06-25 12:07:19 +0200

    create table

--- Auth/__init__.py
+++ Auth/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite3:///../albeton.db'
+db = SQLAlchemy(app)
 
 from Auth import routs
