b2ea288773272fa8adc3e4a022c7b2de8d38cc64
= <=>
2020-06-26 08:24:49 +0200

    edit files

--- Auth/routs.py
+++ Auth/routs.py
@@ -10,19 +10,19 @@ def home():
 
 @app.route('/register',methods=['GET','POST'])
 def register_form():
-    request_data = request.get_json()
-    email = str(request_data["email"])
     
+    request_data = request.get_json()
+    
+    email = str(request_data["email"])
     password = str(request_data["password"])
-    verify_password = str(request_data["verify-password"])
+   
     HP = HashPassword()
     password =  HP.hash_password(password)
-    verify_password =  HP.hash_password(verify_password)
-    if HP.verify_password(password,verify_password):
-        add_user(email,password)
+    newUser= add_user(email,password)
+    newUser.insert_value()
     SendEmail(email)
-    return render_template('register.html')
+    return  'user registered successfully'
 
 @app.route('/login')
 def login_form():
-    return render_template('login.html')
+    return 'user logined successfully'
