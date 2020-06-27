d4927dcb7f282072331413b8baf212f986fa65e7
= <=>
2020-06-27 10:46:59 +0200

    edit routs and init files

--- Auth/__init__.py
+++ Auth/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from Auth.models import CreateTable,Connection
 
 app = Flask(__name__)
-CreateTable.Create_table()
+CreateTable.create_table()
 
 
 from Auth import routs
--- Auth/routs.py
+++ Auth/routs.py
@@ -1,6 +1,6 @@
-from flask import render_template,request, Response, jsonify
+from flask import render_template, request, Response, jsonify, flash, redirect
 from Auth import app
-from Auth.models import add_user,Authentication, UserUpdate
+from Auth.models import AddUser, Authentication, UserUpdate
 from Auth.sendEmail import SendEmail
 from Auth.HashPassword import HashPassword
 import datetime
@@ -8,13 +8,15 @@ from functools import wraps
 import jwt
 
 
-app.config["SECRET_KEY"] = 'meow'
+app.config["SECRET_KEY"] = 'f6a6ec1916a64e3294f4bf45bf183f81'
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register_form():
     # Get data from body of POST mtehod
     request_data = request.get_json()
@@ -22,14 +24,18 @@ def register_form():
     password = str(request_data["password"])
     # convert password to hash format
     HP = HashPassword()
-    password =  HP.hash_password(password)
+    password = HP.hash_password(password)
     # class add user method
-    newUser= add_user(email,password)
+    newUser = AddUser(email, password)
     result = newUser.insert_value()
+    print(result)
     # if registration is successfull then send a activate e-mial
     if result:
         SendEmail(email)
-    return  'user registered successfully'
+        return 'The username was successfully registered'
+    else:
+        return 'The username has already been created'
+
 
 def token_required(f):
     @wraps(f)
@@ -42,26 +48,35 @@ def token_required(f):
             return jsonify({"error": "Need to token to show this page"}), 401
     return wrapper
 
+
 @app.route("/login", methods=["GET", "POST"])
 def get_token():
     request_data = request.get_json()
     email = str(request_data["email"])
     password = str(request_data["password"])
-    auth= Authentication(email,password)
-    result = auth.Check_user_password()
+    auth = Authentication(email, password)
+    result = auth.check_user_password()
     print(result)
-    if result==True:
+    if result == True:
         expiration_date = datetime.datetime.utcnow() + datetime.timedelta(seconds=300)
-        token = jwt.encode({"exp": expiration_date}, app.config["SECRET_KEY"], algorithm="HS256")
+        token = jwt.encode({"exp": expiration_date},
+                           app.config["SECRET_KEY"], algorithm="HS256")
         return token
     else:
-        return 'not ok ' #Response(result, mimetype='applicatio/json')
+        return result
 
-
-@app.route('/active')
+@app.route('/active',methods=['GET','POST'])
 def active_user():
     request_data = request.get_json()
     id = str(request_data["id"])
-    up=UserUpdate()
-    up.upser_active(id,True)
-    return 'User successfully Activated'
+    up = UserUpdate()
+    result = up.upser_active(id)
+    if result:
+        return 'user successfully active'
+    else:
+        return 'Please contact to support'
+
+@app.route('/logout')
+def logout():
+	flash('you logged out successfully', 'success')
+	return redirect('home.html')
