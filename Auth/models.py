5a2214e802ef3f2d913178a04fca4b7b62d8095c
= <=>
2020-06-28 15:13:16 +0200

    add context manager

--- Auth/models.py
+++ Auth/models.py
@@ -3,154 +3,176 @@ import sqlite3
 import os
 from Auth.HashPassword import HashPassword
 
+
 class Singleton(type):
     _instance = None
 
     def __call__(self, *args, **kwargs):
-        if self._instance==None:
-            self._instance= super().__call__()
+        if self._instance == None:
+            self._instance = super().__call__()
         return self._instance
 
+
 class Connection(metaclass=Singleton):
+    DEFAULT_PATH = os.path.join(
+        os.path.dirname(__file__), '../albeton.sqlite3')
+
     # create a default path to connect to and create (if necessary) a database
     # called 'database.sqlite3' in the same directory as this script
-    DEFAULT_PATH = os.path.join(os.path.dirname(__file__), '../albeton.sqlite3')
-    print(DEFAULT_PATH)
-    def sql_connection(self,db_path=DEFAULT_PATH):
-        try:
-            con = sqlite3.connect(db_path, check_same_thread=False)
-            return con
-        except Error:
-            print(Error)
-   
-class CreateTable():
-    connect= Connection()
-    con=connect.sql_connection()
+    def __init__(self):
+        self.con= None
+        self.cursor=None
 
+    def __enter__(self, db_path=DEFAULT_PATH):
+        try:
+            self.con = sqlite3.connect(db_path, check_same_thread=False)
+            self.cursor = self.con.cursor()
+            return self.cursor
+        except Error:
+            return Error
+
+    def __exit__(self, exc_type, exc_val, exc_db):
+        try:
+            self.cursor.close()
+            self.con.commit()
+        except Error:
+            return Error
+        
+
+
+class CreateTable():
     @staticmethod
     def create_table():
-        cursorObj = CreateTable.con.cursor()
-        try:
-            cursorObj.execute(f'CREATE TABLE if not exists users ' + \
-                              f'(id       INTEGER PRIMARY KEY,'    + \
-                              f' email    string(100) UNIQUE,'     + \
-                              f' password string(100) UNIQUE,'     + \
-                              f' active   int,'                    + \
-                              f' Lock     int,'                    + \
-                              f' incorrectPass int )')
-            return True
-        except Error:
-            return Error
-        CreateTable.con.commit()
-
-class AddUser():
-    connect = Connection()
-    con=connect.sql_connection()
-
-    def __init__(self,email,password):
-        self.email=email
-        self.password=password
-        
-    def insert_value(self):
-        # Insert a row of data
-        try:
-            AddUser.con.execute(f' INSERT INTO users ' + \
-                                 f' (email,password,active,lock,incorrectPass) ' + \
-                                 f' VALUES (?,?,?,?,?) '
-                                 ,(self.email,self.password,0,0,0))
-                                 # Save (commit) the changes
-            AddUser.con.commit()
-            return True
-        except:
-            return False
-
-class Authentication():
-    connect = Connection()
-    con=connect.sql_connection()
-    hp = HashPassword()
-
-    def __init__(self,email,password):
-        self.email=email
-        self.password= password
-
-    def check_user_password(self):
-        cursor = self.con.cursor()
-        # select a row from data
-        try:
-            cursor.execute(f' select id,email,password,active,lock,incorrectPass from users where email=?'
-                                 ,(self.email,))
-            rows = cursor.fetchall() 
-            cursor.close()
-
-            if len(rows)>0:
-                id = rows[0][0]
-                password = rows[0][2]
-                isactive=int(rows[0][3])
-                islock=int(rows[0][4])
-                incorrectPass=int(rows[0][5])
-                if Authentication.hp.verify_password(password,self.password):
-                    print('password is correct')
-                    if len(rows)>0 :
-                        if isactive==0:
-                            return 'Account is not active'
-                        elif isactive==1 and islock==0:
-                            return True
-                        elif isactive==1 and islock==1:
-                            return 'Account is Locked'
-                    else:
-                        return 'Account is not exist'
-                else:
-                    # id = int(rows[0][0])
-                    # incorrectPass=int(rows[0][5])
-                    uuip= UserUpdate()
-                    uuip.user_incorrectpass(id,incorrectPass)
-                    return 'password is not correct!'
-
-        except Error:
-            return Error
-
-class UserUpdate():
-    connect = Connection()
-    con=connect.sql_connection()
-
-    def upser_active(self,id):
-        cursor = self.con.cursor()
-        # select a row from data
-        try:
-            cursor.execute(f' update users set active = 1 where id = ? ',(id,))
-            self.con.commit()
-            cursor.close()
-            return True
-        except Error:
-            return Error
-        
-    
-    def user_lock(self,id,lock):
-        cursor = UserUpdate.con.cursor()
-        # select a row from data
-        try:
-            cursor.execute(f' update users set lock = ? where id = ? ',(lock,id))
-            UserUpdate.con.commit()
-            cursor.close()
-        except Error:
-            return Error
-        
-
-    def user_incorrectpass(self,id,incorrectPass):
-        cursor = UserUpdate.con.cursor()
-        # select a row from data
-        incorrectPass+=1
-        # hhhhhhhhhhhaaaaaaaaaaaaaaaaaaaaaaarrrrrrrrrrrrrdddddddd code
-        if incorrectPass>3:
-            ss = UserUpdate()
-            ss.user_lock(id,1)
-        else:
+        with Connection() as cursor:
             try:
-                cursor.execute(f'  UPDATE users set incorrectPass = ? where id = ? ',(incorrectPass,id))
-                UserUpdate.con.commit()
-                cursor.close()
+                cursor.execute(f'CREATE TABLE if not exists users ' +
+                                f'(id       INTEGER PRIMARY KEY,' +
+                                f' email    string(100) UNIQUE,' +
+                                f' password string(100) UNIQUE,' +
+                                f' active   int,' +
+                                f' Lock     int,' +
+                                f' incorrectPass int )')
+                return True
             except Error:
                 return Error
+         
+
+
+class AddUser():
+    def __init__(self, email, password, active, lock, incorrectPass):
+        self.email = email
+        self.password = password
+        self.active = active
+        self.lock = lock
+        self.incorrectPass = incorrectPass
+
+    def insert_user(self):
+        with Connection() as cursor:
+            # Insert a row of data
+            try:
+                cursor.execute(f' INSERT INTO users ' +
+                                    f' (email,password,active,lock,incorrectPass) ' +
+                                    f' VALUES (?,?,?,?,?) ', (self.email, self.password, self.active, self.lock, self.incorrectPass))
+                # Save (commit) the changes
+                return True
+            except:
+                return False
+
+
+class Authentication():
+    hp = HashPassword()
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def check_user_password(self):
+        with Connection() as cursor:
+            # select a row from data
+            try:
+                cursor.execute(
+                    f' select id,email,password,active,lock,incorrectPass from users where email=?', (self.email,))
+                rows = cursor.fetchall()
+
+                if len(rows) > 0:
+                    id = rows[0][0]
+                    password = rows[0][2]
+                    isactive = int(rows[0][3])
+                    islock = int(rows[0][4])
+                    incorrectPass = int(rows[0][5])
+
+                    if Authentication.hp.verify_password(password, self.password):
+                        print('password is correct')
+                        print(rows)
+                        if len(rows) > 0:
+                            if isactive == 0:
+                                return 'Account is not active'
+                            elif isactive == 1 and islock == 0:
+                                return True
+                            elif isactive == 1 and islock == 1:
+                                return 'Account is Locked'
+                        else:
+                            return 'Account is not exist'
+                    else:
+                        uuip = UserIncorrectPass(id, incorrectPass)
+                        uuip.user_update()
+                        return 'password is not correct!'
+
+            except Error:
+                return Error
+
+from abc import ABC,abstractclassmethod
+
+class UserUpdate:
+    @abstractclassmethod
+    def user_update(self):
+        pass
+
+class UpserActive(UserUpdate):
+    def __init__(self,id):
+        self.id=id
+        
+    def user_update(self):
+        with Connection() as cursor:
+            # select a row from data
+            try:
+                cursor.execute(
+                    f' update users set active = 1 where id = ? ', (self.id,))
+                return True
+            except Error:
+                return Error
+
+class UserLock(UserUpdate):
+    def __init__(self,id,lock):
+        self.id=id
+        self.lock=lock
+
+    def user_update(self):
+        with Connection() as cursor:
+            # select a row from data
+            try:
+                cursor.execute(f' update users set lock = ? where id = ? ',(self.lock,self.id))
+            except Error:
+                return Error
+        
+class UserIncorrectPass(UserUpdate):
+    def __init__(self,id,incorrectPass):
+        self.id=id
+        self.incorrectPass=incorrectPass
+
+    def user_update(self):
+        with Connection() as cursor:
+            # select a row from data
+            self.incorrectPass+=1
+            # hhhhhhhhhhhaaaaaaaaaaaaaaaaaaaaaaarrrrrrrrrrrrrdddddddd code
+            if self.incorrectPass > 3:
+                lock_account = UserLock(self.id,1)
+                lock_account.user_update()
+            else:
+                try:
+                    cursor.execute(f'  UPDATE users set incorrectPass = ? where id = ? ',(self.incorrectPass,self.id))
+                except Error:
+                    return Error
 
 class UserDelete():
     pass
